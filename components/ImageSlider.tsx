d08f51b080052024d6723f5ffb17ae8cf4dd7aef
eliyamelamed1 <eliyamelamed1@email.com>
2022-06-09 20:02:38 +0300

    removed controls from video tag

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -1,11 +1,11 @@
 import 'swiper/css';
 import 'swiper/css/pagination';
 
+import React, { useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import Image from 'next/image';
 import { Pagination } from 'swiper';
-import React from 'react';
 import styles from '../styles/components/ImageSlider.module.scss';
 
 interface PropTypes {
@@ -14,6 +14,7 @@ interface PropTypes {
 }
 
 export default function ImageSlider({ videoArray, image }: PropTypes) {
+    useEffect;
     return (
         <div className={styles.container}>
             <Swiper
@@ -37,7 +38,7 @@ export default function ImageSlider({ videoArray, image }: PropTypes) {
                 {videoArray.map((imageSrc, index) => {
                     return (
                         <SwiperSlide key={index}>
-                            <video loop controls>
+                            <video loop autoPlay>
                                 <source src={imageSrc} type='video/mp4' />
                             </video>
                         </SwiperSlide>
